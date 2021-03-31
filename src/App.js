b26075c9ae6c1503c4496070aa363177be8bdf5b
Jorge <djsmoothzouk@gmail.com>
2021-03-31 17:27:02 -0500

    accidentally did this in dev branch! added color theme and created globalstyle component along with venue component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import Venue from'./components/Venue'
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStlye = createGlobalStyle`
+body {
+  background: #070054;
+  text-align: center;
+}
+
+h1 {
+  color: #ff49f2;
+}
+`
 
 function App() {
   return (
     <div className="App">
+      <GlobalStlye />
+      <h1>Home Page</h1>
+      <Venue />
     </div>
   );
 }
---
+++ src/components/Venue.js
@@ -0,0 +1,10 @@
+import React from'react';
+
+
+const Venue = () => {
+    return (
+        <h1>Venue Page</h1>
+    )
+}
+
+export default Venue
